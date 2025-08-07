f5cb0eaded907f8fbac5b1c7fae6bbf995af714d
Jayachandra2419 <gjayachandra2419@gmail.com>
2025-08-07 16:44:11 +0530

    Create AdminPanel.js

---
+++ src/components/AdminPanel.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function AdminPanel() {
+  return (
+    <div style={{padding: 20}}>
+      <h2>Admin Panel (Coming soon)</h2>
+      <p>Admins can approve or reject listings here.</p>
+    </div>
+  );
+}
