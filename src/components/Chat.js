07d3003e0bacacfb3cb72cd8d96c0559beee5094
Jayachandra2419 <gjayachandra2419@gmail.com>
2025-08-07 16:43:37 +0530

    Create Chat.js

---
+++ src/components/Chat.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Chat() {
+  return (
+    <div style={{padding: 20}}>
+      <h2>Chat (Coming soon)</h2>
+      <p>This is where buyers and sellers can chat about listings.</p>
+    </div>
+  );
+}
